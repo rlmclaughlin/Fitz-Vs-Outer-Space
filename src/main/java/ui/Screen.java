b902ad32a901862d101d76a72be63ba325b6a224
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2021-03-27 19:53:52 -0500

    Created basic frame and window for game. Thread is running and providing and providing updates

--- src/main/java/ui/Screen.java
+++ src/main/java/ui/Screen.java
@@ -1,4 +1,24 @@
 package ui;
 
-public class Screen {
+import javax.swing.*;
+
+public class Screen extends JPanel implements Runnable {
+
+    private int i = 0;
+    private Thread thread;
+
+    public Screen(){
+        thread = new Thread(this);
+    }
+
+    public void startGame(){
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            System.out.println("updates/ticks: " + i++);
+        }
+    }
 }
--- src/main/java/ui/Window.java
+++ src/main/java/ui/Window.java
@@ -1,4 +1,26 @@
 package ui;
 
-public class Window {
+import javax.swing.*;
+
+public class Window extends JFrame {
+
+    private Screen screen;
+
+    public Window(){
+        super("Fitz V.S. Outer Space!");
+        setSize(800, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        screen = new Screen();
+        add(screen);
+    }
+
+    public void startGame(){
+        screen.startGame();
+    }
+
+    public static void main(String[] args){
+        Window window = new Window();
+        window.setVisible(true);
+        window.startGame();
+    }
 }
